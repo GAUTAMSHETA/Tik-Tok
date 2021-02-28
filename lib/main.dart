6613000213b7b4eb75c2c4c5d8f66de010c91732
GAUTAMSHETA <78837362+GAUTAMSHETA@users.noreply.github.com>
2021-02-28 20:18:13 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_app/pages/root_app.dart';
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
